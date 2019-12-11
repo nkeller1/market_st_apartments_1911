c3981b291dad8037110497baf8c4efe19971207c
Nathan Keller <keller.nathan@gmail.com>
2019-12-11 09:16:17 -0700

    Work on add_renter to apartment

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,1 +1,15 @@
+class Apartment
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
 
+  def initialize(apartment_specs, renter = nil)
+    @number = apartment_specs[:number]
+    @monthly_rent = apartment_specs[:monthly_rent]
+    @bathrooms = apartment_specs[:bathrooms]
+    @bedrooms = apartment_specs[:bedrooms]
+    @renter = renter
+  end
+
+  def add_renter(renter)
+
+  end
+end
---
+++ test/renter_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter'
+require './lib/apartment'
+
+class RenterTest < Minitest::Test
+
+def test_it_exists
+  renter1 = Renter.new("Jessie")
+  assert_instance_of Renter, renter1
+end
+
+def test_it_has_a_name
+  renter1 = Renter.new("Jessie")
+  assert_equal "Jessie", renter1.name
+end
+
+end
